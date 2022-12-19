93ff7cbcc8592535284f220043748a86a27408d2
TAMWO FEUWO FRANCK VALERE <valeretamwo@gmail.com>
2022-12-19 21:52:57 +0100

    Ajout du titre

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
-# Find out more about building applications with Shiny here:
+# Find out more about building applications with Shiny here:  
 #
 #    http://shiny.rstudio.com/
 #
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Client Region Prediction"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
