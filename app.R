dd977bc9b78570deb1b1ce2d04f067ade4078ca7
TAMWO FEUWO FRANCK VALERE <valeretamwo@gmail.com>
2022-12-21 22:06:56 +0100

    Addind footer

--- app.R
+++ app.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(shiny.tailwind)
+library(shinydashboard)
 options(shiny.launch.browser = .rs.invokeShinyWindowExternal)
 
 # Define UI for application that draws a histogram
@@ -19,13 +20,21 @@ ui <- fluidPage(
     tags$div(class='btn-l',
       tags$button('Sign In',class='btn text-[arial]',style="color:#ab53dd; font-family:'arial';"),
       tags$button('Log In',style="font-family:'arial';",class='text-[whitesmoke] bg-[#8c07da] h-[40px] w-[100px] mr-[50px] border-[1px] p-[10px] text-[15px] rounded-[4px] mt-[10px] text-[arial]',style="font-family:'arial';")))
+  ,
+  dashboardSidebar()
   ,tabsetPanel(
-    tabPanel("Home",tags$div(img(src='undraw_code_thinking_re_gka2.svg',style="height:400px;width:400px;position:relative;"),class='image',tags$p('Welcome to Data World with R-Shiny',class='p-text'))
+    tabPanel("Home",sidebarLayout(sidebarPanel('WholeSale Statistics'),mainPanel('data')),tags$div(img(src='undraw_code_thinking_re_gka2.svg',style="height:400px;width:400px;position:relative;"),class='image',tags$p('Welcome to Data World with R-Shiny',class='p-text'))
   ),
-  tabPanel("data",dataTableOutput('data')
-           )
+  tabPanel(class="text-[arial]" ,"Data",dataTableOutput('data')
+           ),
+  tabPanel("About",
+           sidebarPanel('Why is this WholeSale Customers Data has been collected?'),
+           tags$div(class='flex position relative right-[400px] gap-[30px]', tags$div(class='w-[40vw] h-[55vh] border-[1px] rounded-[20px] justify-center items-center relative left-[9%] top-[70px]'),
+                    tags$div(class='w-[40vw] h-[55vh] border-[1px] rounded-[20px] justify-center items-center relative left-[9%] top-[70px]')),
+  ),tags$footer(class='w-[98vw] p-[100] h-[70px] bg-[#8c07da] relative top-[155px] text-[whitesmoke]',tags$div(class='flex  space-around flex-row', tags$p('Copyright 2022'),tags$p('DataMining'),tags$p('Classification Model')
+                                                                                                               )
+                )
   ))
-
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
